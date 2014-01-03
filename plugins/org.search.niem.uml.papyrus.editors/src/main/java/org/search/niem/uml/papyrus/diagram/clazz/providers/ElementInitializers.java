e893a667f50e8066982299eb96cb829988cfbe98
timezra <timezra@gmail.com>
2014-01-02 18:27:20 -0800

    Adding standard NIEM UML headers; re-enabling marker quick-fix extension points.

--- plugins/org.search.niem.uml.papyrus.editors/src/main/java/org/search/niem/uml/papyrus/diagram/clazz/providers/ElementInitializers.java
+++ plugins/org.search.niem.uml.papyrus.editors/src/main/java/org/search/niem/uml/papyrus/diagram/clazz/providers/ElementInitializers.java
@@ -1,3 +1,13 @@
+/*
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   SEARCH Group, Incorporated - initial API and implementation
+ *
+ */
 package org.search.niem.uml.papyrus.diagram.clazz.providers;
 
 import static org.search.niem.uml.papyrus.preferences.NIEMPreferenceConverter.getAggregationKind;
